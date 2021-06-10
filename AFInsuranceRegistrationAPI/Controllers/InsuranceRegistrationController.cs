847c2c5c485a996ff388665e168ee8724f033142
Satish Chittibomma <sc32762@open.ac.uk>
2021-06-10 16:46:12 +0100

    Intial Project set up with required classes and Model

---
+++ AFInsuranceRegistrationAPI/Controllers/InsuranceRegistrationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AFInsuranceRegistrationAPI.Controllers
+{
+    public class InsuranceRegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
