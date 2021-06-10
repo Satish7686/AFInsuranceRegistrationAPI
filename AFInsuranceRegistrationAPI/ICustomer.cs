60b2195dadfedf9a0c4274a23b64f1148fc36c45
Satish Chittibomma <sc32762@open.ac.uk>
2021-06-10 18:51:55 +0100

    Interface implementation and Data validation

---
+++ AFInsuranceRegistrationAPI/ICustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AFInsuranceRegistrationAPI
+{
+    interface ICustomer
+    {
+        string FirstName { get; set; }
+        string SurName { get; set; }
+        string PolicyReferenceNumber { get; set; }
+        DateTime DateOfBirth { get; set; }
+        string PolicyHolderEmailAddress { get; set; }
+
+    }
+}
--- AFInsuranceRegistrationAPI/Models/CustomerModel.cs
+++ AFInsuranceRegistrationAPI/Models/CustomerModel.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AFInsuranceRegistrationAPI.Models
 {
-    public class CustomerModel
+    public class CustomerModel : ICustomer
     {
+
+      // First Name Validation minimun 3 charectors max 50 charectors
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        // SurName Validation minimun 3 charectors max 50 charectors
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string SurName { get; set; }
+
+        // Policy Reference validation accepting two capitalised alpha character followed by a hyphen and 6 numbers
+        [Required]
+        [RegularExpression(@"^[A-Z]{2}[-]\d{6} *$")]
+        public string PolicyReferenceNumber { get; set; }
+        
+       
+        public DateTime DateOfBirth { get; set; }
+
+        // Email validation accepting a string of at least 4 alpha numeric chars followed by an ‘@’ sign and then another string of at least 2 alpha numeric chars.The email address should end in either ‘.com’ or ‘.co.uk’
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]{4,99}@[a-zA-Z0-9-]{2,99}\.[\.com or co\.uk]+$")]
+        public string PolicyHolderEmailAddress { get; set; }
+
     }
 }
